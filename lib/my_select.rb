54c58e82b52a56b53016566e8d6f449ae4d54753
Matt Maher <mattmaher0@gmail.com>
2018-05-15 14:26:40 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
+  count = 0
+  selected = []
+  while count < collection.length
+    if collection[count] == 2 || collection[count] == 4
+    yield collection[count] 
+    selected << collection[count]
+    end
+  count += 1    
+  end
+  selected
 end
