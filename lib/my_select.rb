e4e7ac8074186638ad2dcd32a9e2c55ad2ae7351
Wolfgang Pfretzschner <wolfgangp@live.com>
2018-05-24 09:33:19 +0200

    done

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
 def my_select(collection)
- # your code here!
+  if collection.length == 0
+    false
+  else
+    new_collection = []
+    i = 0
+    while i < collection.length
+      if yield(collection[i]) == true
+        new_collection.push(collection[i])
+      end
+      i+=1
+    end
+    new_collection
+  end
 end
