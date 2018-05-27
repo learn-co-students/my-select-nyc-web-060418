d681b01e41d1d3ac4463b5134bcc862db48e0615
KoichiSakamoto <isaac.fiore@mail.mcgill.ca>
2018-05-27 17:50:25 -0400

    implemented select using yield

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
  # your code here!
+ i = 0
+ returnArray = []
+ while i < collection.length
+   if yield(collection[i])
+     returnArray.push(collection[i])
+   end
+   i += 1
+ end
+ returnArray
 end
