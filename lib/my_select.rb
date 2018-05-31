38fa27424f97a097a11e5afa0f3975c839bbd38d
Shing Wong <shing2487@gmail.com>
2018-05-31 18:38:08 -0400

    completed my select lab

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,18 @@
 def my_select(collection)
  # your code here!
+ if block_given?
+   i = 0
+   new_array = []
+
+   while i < collection.length
+     if yield(collection[i]) == true
+       new_array << collection[i]
+     end
+     i += 1
+   end
+   new_array
+ else
+   "This block should not run!"
+ end
+
 end
